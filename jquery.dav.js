a270121df406515329552b0488235c6994960499
Sandro Pasquali <spasquali@gmail.com>
2011-12-16 06:58:22 -0500

    updates

--- jquery.dav.js
+++ jquery.dav.js
@@ -1,24 +1,22 @@
-/*
-* jDav JQuery plugin v1.0
-*
-* Copyright 2008 Lime Labs LLC
-*
-* @author Sandro Pasquali (spasquali@gmail.com)
-*
-* jDav is free software: you can redistribute it and/or modify
-* it under the terms of the GNU Lesser General Public License
-* version 3 as published by the Free Software Foundation.
-*
-* jDav is distributed in the hope that it will be useful,
-* but WITHOUT ANY WARRANTY; without even the implied warranty of
-* MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-* GNU Lesser General Public License for more details.
-*
-* You should have received a copy of the GNU Lesser General Public
-* License along with jDav.  If not, see <http://www.gnu.org/licenses/>.
-*/
-
-
+/* ============================================================
+ * jquery.dav.js v1.0.0
+ * https://github.com/sandro-pasquali/jquery.dav
+ * ============================================================
+ * Copyright (c) 2011 Sandro Pasquali (spasquali@gmail.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ============================================================ */
+ 
 (function($) {
 
   /**
